d2d78c05c46b956e4f647a2d4fc77dcf32d2817b
Asa_zr <926197502@qq.com>
2022-03-24 21:52:26 +0800

    chore:tsday

---
+++ index.ts
@@ -0,0 +1,70 @@
+//使用class关键字定义类
+class Person{
+    //定义类的属性 实例属性需要通过对象的实例来访问
+    //readonly 只读属性
+    name:string;
+    age:number;
+    //在属性前使用static关键字，表示这个属性是类的属性，而不是实例属性，可以通过类名来访问
+    static sayHi():string{
+        return 'Hi';
+    }
+    
+    //定义类的构造函数
+    constructor(name:string,age:number){
+        this.name=name;
+        this.age=age;
+    }
+    //定义类的方法
+    sayHello():string{
+        return `Hello,${this.name}`;
+    }
+}
+const per=new Person("张三",20);
+console.log(per);
+
+// class Dog{
+//   name:string;
+//   constructor(name:string){
+//     this.name=name;
+//   }
+//   bark(){
+//     console.log(this.name+" is barking");
+//   }
+// }
+// const dog=new Dog("小黑");
+
+class Animal{
+    name:string;
+    constructor(name:string){
+        this.name=name;
+    }
+    //定义类的方法
+    sayHello(){
+        console.log(`Hello,${this.name}`);
+        
+    }
+}
+class Dog extends Animal{
+    constructor(name:string){
+        super(name);
+    }
+    //重写父类的方法
+    sayHello(){
+        console.log(this.name+" is saying hello");
+        
+    }
+}
+class Cat extends Animal{
+    constructor(name:string){
+        super(name);
+    }
+    sayHello(){
+        console.log(this.name+" is saying hello");
+        
+    }
+}
+const dog=new Dog("小黑");
+const cat=new Cat("小白");
+dog.sayHello();
+cat.sayHello();
+
