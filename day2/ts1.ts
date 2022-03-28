17d743cb9d7b8fffada87e699f79f6ed8adc0a72
Asa_zr <926197502@qq.com>
2022-03-28 19:59:27 +0800

    chore:抽象类

--- day2/ts1.ts
+++ day2/ts1.ts
@@ -47,5 +47,26 @@ class Cat{
 }
 const cat = new Cat("小白",3);
 console.log(cat._name,cat._age);
-
+//抽象类的定义
+abstract class Tree{
+    abstract name:string;
+    abstract age:number;
+    abstract sayHello():void;
+}
+class LemonTree extends Tree{
+    name:string;
+    age:number;
+    constructor(name:string,age:number){
+        super();
+        this.name=name;
+        this.age=age;
+    }
+    sayHello(){
+        console.log("Hello, " + this.name);
+    }
+}
+const tree = new LemonTree("小柠檬",3);
+console.log(tree.name,tree.age);
+tree.sayHello();
+// const tree=new Tree();//抽象类无法被实例化
 
